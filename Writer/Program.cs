b50951a17dffa0340d7f84e6d7a479eddcb132a9
Dan Clarke <dan@everstack.com>
2023-01-19 07:22:15 +0000

    Initial commit

---
+++ Writer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using Serilog;
+using Serilog.Sinks.Network;
+
+var ip = IPAddress.Parse("127.0.0.1");
+var logger = new LoggerConfiguration()
+    .WriteTo.TCPSink(ip, 1337)
+    .CreateLogger();
+
+logger.Information($"HELLO 4!");
+logger.Information($"HELLO 5!");
+logger.Information($"HELLO 3!");
+await Task.Delay(2000);
+
